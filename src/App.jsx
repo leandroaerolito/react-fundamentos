7a35afbe825a119bcace0547666b67df90107206
Leandro Rocha <leandro.aerolito@gmail.com>
2023-11-29 11:57:18 -0300

    Separação dos Componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,18 @@
 // Comentário de uma linha CTRL; //
 /* Comentário de várias linhas ALT SHIFT A */
 
-import Cabecalho from "./cabecalho";
+import Conteudo from "./components/Conteudo";
+import Rodape from "./components/Rodape";
+import Cabecalho from "./components/Cabecalho";
 
 function App() {
   return (
     <>
       <Cabecalho />
-      <main>
-        <section>
-          <h2>Conteúdo de aplicação</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
-            quisquam officia sit, cum modi labore voluptatibus quam, possimus
-            illum iure ut dicta quo atque odit impedit quasi hic voluptates ex
-            soluta! Distinctio deleniti aspernatur vel aperiam sunt? Odit
-            perferendis similique nobis fugiat iusto nulla, qui incidunt
-            voluptas hic, fuga eveniet.
-          </p>
 
-          <article>
-            <h3>Artigo 1</h3>
-            <p>Conteúdo do artigo 1...</p>
-          </article>
+      <Conteudo />
 
-          <article>
-            <h3>Artigo 2</h3>
-            <p>Conteúdo do artigo 2....</p>
-          </article>
-
-          <article>
-            <h3>Artigo 3</h3>
-            <p>Conteúdo do artigo 3...</p>
-          </article>
-        </section>
-      </main>
-
-      <footer>
-        <h2>Rodapé do aplicativo</h2>
-      </footer>
+      <Rodape />
     </>
   );
 }
